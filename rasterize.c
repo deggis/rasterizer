bc94851e934e558502f8371825833ca18180fff9
Heikki Salo <heikki.ao.salo@iki.fi>
2012-09-26 13:55:02 +0300

    Progress.
    Signed-off-by: Heikki Salo <heikki.ao.salo@iki.fi>

--- rasterize.c
+++ rasterize.c
@@ -15,7 +15,16 @@ typedef struct
     Point p;
     struct PointNode *next;
 } PointNode;
-    
+
+typedef struct
+{
+    double lu_long;
+    double lu_lat;
+    double rl_long;
+    double rl_lat;
+    double gsd; // in metres
+} TaskInfo;
+
 
 // void append(Point** node, Point* new) {
 //     if( *node == 0 ) {
@@ -34,8 +43,38 @@ typedef struct
 // }
 // 
 
+
+
+
+/*
+ * Long/lat's are projected to pixel centers.
+ *
+ * h = rows-1, w = cols-1
+ * 
+ * 
+ *
+ *
+ *            LONG / LAT                 
+ *        ^                                    0,0 |             w-1,0
+ *    lat |    lu-----+                      ------+------------------
+ *        |    |      |        ======>             |
+ *        |    +-----rl                            |
+ *        |                                        |
+ *    ----+----------------->                0,h-1 |
+ *    0,0 |             long                                           
+ *                                              lu -> 0,0
+ *        ^-- greenwich                         rl -> 
+ *                                      
+ */
+
+void transform(TaskInfo *info, int *px, int *py) {
+    *px = 
+}
+
 int main(int argc, char **argv) {
     printf("Jotain\n");
+
+    TaskInfo info = { 24.0, 65.0, 25.0, 66.0, 0.05 };
 
     int rows=5, //9175,
         cols=5, //6814,
@@ -52,11 +91,11 @@ int main(int argc, char **argv) {
     }
 
     // data
-    Point a = { 1.7, 2.7, 8 };
-    Point b = { 1.2, 2.2, 6 };
-    Point c = { 1.4, 3.2, 5 };
-    Point d = { 1.4, 3.2, 5 };
-    Point e = { 2.5, 4.5, 3 };
+    Point points[5] = { 1.7, 2.7, 8 } 
+            Point b = { 1.2, 2.2, 6 }
+            Point c = { 1.4, 3.2, 5 }
+            Point d = { 1.4, 3.2, 5 }
+            Point e = { 2.5, 4.5, 3 }
     printf("Size: %d\n", sizeof(PointNode));
 //
 //    addPoint(nodes, rows, cols, &a);
